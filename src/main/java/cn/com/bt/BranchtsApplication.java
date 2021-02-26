681118f1fd1988199761bb6a0f44b28a0523497b
zhaoxin <15237139190>
2021-02-26 09:18:31 +0800

    Initial commit

---
+++ src/main/java/cn/com/bt/BranchtsApplication.java
@@ -0,0 +1,13 @@
+package cn.com.bt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/com/bt/BranchtsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.bt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
